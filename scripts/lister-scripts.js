d55b1dd694180a1ca4e80589a1ea3495dd1cbab8
Gracen Shepherd <gracendavidshepherd@gmail.com>
2025-10-23 18:07:31 -0500

    Add form, improve map Lender's form complete Use overflow to scroll, account for in JS

--- scripts/lister-scripts.js
+++ scripts/lister-scripts.js
@@ -1,16 +1,28 @@
 const container = document.querySelector(".map");
 let dot = document.querySelector(".dot")
 containerRect = container.getBoundingClientRect();
+let coordsElement = document.querySelector(".coords");
 
+function TruncateSixDigits(num) {
+    return Math.round(num * (10 ** 6)) / (10 ** 6)
+}
 
 container.addEventListener('click', (event) => {
-
-    dot.style.left = String(event.pageX - (containerRect.x + 10)) + "px";
-    dot.style.top = String(event.pageY - (containerRect.y + 10)) + "px";
+    dot.style.left = String(event.pageX + container.scrollLeft - (containerRect.x + 5)) + "px";
+    dot.style.top = String(event.pageY + container.scrollTop - (containerRect.y + 5)) + "px";
     dot.style.opacity = "50%";
-    currentCoords[0] = coords[0] + (event.pageX - (containerRect.x + 10)) * 0.001;
-    currentCoords[1] = coords[1] + (event.pageY - (containerRect.y + 10)) * 0.001;
+    currentCoords[1] = coords[1] + (event.pageX + container.scrollLeft - (containerRect.x + 5)) * 0.00001;
+    currentCoords[0] = coords[0] - (event.pageY + container.scrollTop - (containerRect.y + 5)) * 0.00001;
+
+    coordsElement.textContent = '' + TruncateSixDigits(currentCoords[0]) + ' ' + TruncateSixDigits(currentCoords[1]);
 })
 
 const coords = [30.42454333155956, -91.08618485649713]
-let currentCoords = coords;
+let currentCoords = [...coords];
+
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener('click', (event) => {
+    currentCoords = [...coords];
+    coordsElement.textContent = '(none selected)';
+    dot.style.opacity = '0';
+})
